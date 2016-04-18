924b3086129d4d86210b1ab681a106a313936520
ginberg <ginberg@gmail.com>
2016-04-18 20:57:08 +0200

    update dashboard3

--- dashboard3.R
+++ dashboard3.R
@@ -29,7 +29,7 @@ ui <- dashboardPage(
       tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
     ),
     tabsetPanel(
-      tabPanel("Tab1", plotOutput("totalBR", height = 400, width = 1200)), 
+      tabPanel("Tab1", plotOutput("totalBR", height = 400, width = 1200, hover = hoverOpts(id ="plot_hover")), verbatimTextOutput("hover_info")), 
       tabPanel("Tab2", htmlOutput("totalBRvis"))
       )
     
@@ -46,6 +46,8 @@ server <- function(input, output) {
   BRs <- BRs[order(BRs)]
   DEs <- unique(po_current_errors$Data.Element)
   DEs <- DEs[order(DEs)]
+  
+  #df <- "old"
   
   output$sliderBR <- renderUI({
      ticks  <- c(10,20,30)
@@ -90,12 +92,46 @@ server <- function(input, output) {
     df <- melt(df[,c('BR', rev(input$types))],id.vars = 1)
     df$variable <- factor(df$variable, levels = rev(levels(df$variable)))
     colnames(df) <- c("BR", "category", "value")
+    assign("df", df, envir = .GlobalEnv)
     if(nrow(df) >0){
       ggplot(data=df, aes(x = BR, y= value, fill = category, order = as.numeric(category))) + geom_bar(stat="identity") +
         ggtitle("Totale huidige uitval per BR") + xlab("Business Rule nummer") + ylab("Aantal regels") +
         scale_fill_manual(values = c("whitelisted" = "gray", "failed" = "red", "confirmed" = "blue", "unhandled" = "orange"))
     } else{
       return()
+    }
+  })
+  
+  output$hover_info <- renderUI({
+    hover <- input$plot_hover
+    if(!is.null(hover)){
+      point <- nearPoints(df, hover)
+      if (nrow(point) == 0) return(NULL)
+      print(point)
+      
+      # calculate point position INSIDE the image as percent of total dimensions
+      # from left (horizontal) and from top (vertical)
+      left_pct <- (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)
+      top_pct <- (hover$domain$top - hover$y) / (hover$domain$top - hover$domain$bottom)
+      
+      # calculate distance from left and bottom side of the picture in pixels
+      left_px <- hover$range$left + left_pct * (hover$range$right - hover$range$left)
+      top_px <- hover$range$top + top_pct * (hover$range$bottom - hover$range$top)
+      
+      # create style property fot tooltip
+      # background color is set so tooltip is a bit transparent
+      # z-index is set so we are sure are tooltip will be on top
+      style <- paste0("position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.85); ",
+                      "left:", left_px + 2, "px; top:", top_px + 2, "px;")
+      
+      # actual tooltip created as wellPanel
+      wellPanel(
+        style = style,
+        p(HTML(paste0("<b> Car: </b>", rownames(point), "<br/>",
+                      "<b> mpg: </b>", point$mpg, "<br/>",
+                      "<b> hp: </b>", point$hp, "<br/>",
+                      "<b> Distance from left: </b>", left_px, "<b>, from top: </b>", top_px)))
+      )
     }
   })
 }
